a9c0ae424e9bbadbbd1bb197f9d16a7f05fba2a3
Brady Trainor <mail@bradyt.com>
2017-06-18 15:47:11 -0700

    A little work towards installing pypi

---
+++ README.rst
@@ -0,0 +1,5 @@
+
+This CLI tool assists in setup of the TaskServer tool `taskd`. It
+helps in initializing taskd and adding users. Besides this, an effort
+will also be made to make (multiple) client configuration smoother,
+and easy to do after the fact.
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 
 from setuptools import setup
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name = 'taskdsetup',
-    version = '0.0.1',
+    version = '0.0.1.dev2',
+    description = 'CLI to assist in TaskServer setup',
+    url = 'https://github.com/bradyt/taskdsetup',
+    author = 'Brady Trainor',
+    author_email = 'mail@bradyt.com',
+    license = 'GPLv3',
+    long_description = long_description,
     entry_points = {
         'console_scripts': ['taskdsetup=taskdsetup.application:main'],
-    }
+    },
 )
